f0d9c6d22ac453d81527e175f0c6320bb412eeb0
amrudesh1 <amrbal@gmail.com>
2020-01-21 09:43:05 +0530

    Added Button Listeners

---
+++ appupdater/src/main/java/com/amrudesh/appupdater/model/BindingModel.kt
@@ -0,0 +1,11 @@
+package com.amrudesh.appupdater.model
+
+/**
+ * Created by Amrudesh Balakrishnan.
+ */
+class BindingModel(val name: String) {
+    var title: String = " "
+    var content: String = " "
+    var negativeButtonText: String = " "
+    var positiveButtonText: String = " "
+}
